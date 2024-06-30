843aa5938f469dc75968e8fc407363545887903f
Sandip  Pradhan <158505928+sandip9851@users.noreply.github.com>
2024-06-30 20:20:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
-const animal = new Animal("Unknown");
-console.log(animal.species); 
-animal.makeSound(); 
+//complete this code
+class Animal {
+	constructor(species){
+		this.type=species;
+	}
+	get species(){
+		return this.type;
+	}
+	makeSound(){
+		console.log("The "+this.species+" makes a sound")
+	}
+}
+
+class Cat extends Animal {
+	purr(){
+		console.log("purr");
+	}
+}
+
+class Dog extends Animal {
+	bark(){
+		console.log("woof")
+	}
+}
+///
 // const animal = new Animal("Unknown");
 // console.log(animal.species); 
 // animal.makeSound(); 
 
-const cat = new Cat("Cat");
-console.log(cat.species); 
-cat.makeSound(); 
-cat.purr();
 // const cat = new Cat("Cat");
 // console.log(cat.species); 
 // cat.makeSound(); 
 // cat.purr();
 
-const dog = new Dog("Dog");
-console.log(dog.species); 
-dog.makeSound(); 
-dog.bark(); 
 // const dog = new Dog("Dog");
 // console.log(dog.species); 
 // dog.makeSound(); 
@@ -25,3 +39,5 @@ dog.bark();
 
 // Do not change the code below this line
 window.Animal = Animal;
+window.Dog = Dog;
+window.Cat = Cat;
